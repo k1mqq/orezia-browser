569e288d2d6efadf57fa39a2ebb915e82b77d863
k1mqq <keemacurry238@gmail.com>
2026-06-19 11:24:20 +0900

    DTC: Dakyo Tree Construct

--- src/main.rs
+++ src/main.rs
@@ -16,8 +16,17 @@ fn main()  -> Result<(), Box<dyn Error>> {
     println!("{}", &response.status);
     println!("{}", &response.body);
 
-    let tokens = html_parser::Tokenizer::new(&response.body).tokenize();
+    let mut tokenizer = html_parser::Tokenizer::new(&response.body);
+    let mut tree_builder = html_parser::TreeConstructor::new();
 
-    println!("{:?}", &tokens);
+    loop {
+        let token = tokenizer.next_token();
+        tree_builder.process_token(&token);
+        if matches!(token, html_parser::Token::Eof) {
+            break;
+        }
+    }
+
+    println!("{:?}", &tree_builder.dom);
     Ok(())
 }
