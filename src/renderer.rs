cf4f26057984c2344bf1c836884cf9e11c1bc2d2
k1mqq <keemacurry238@gmail.com>
2026-06-21 11:12:45 +0900

    renderer::renderer

--- src/renderer.rs
+++ src/renderer.rs
@@ -4,16 +4,23 @@ use std::sync::Arc;
 use fontdue::Font;
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
-use winit::event_loop::{self, ActiveEventLoop, ControlFlow, EventLoop};
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
 use winit::window::Window;
 
-struct App {
+struct Renderer {
     font: Font,
+    buffer: Vec<u32>,
+    width: u32,
+    height: u32,
+}
+
+struct App {
+    renderer: Option<Renderer>,
     state: Option<(Arc<Window>, softbuffer::Surface<Arc<Window>, Arc<Window>>)>,
 }
 
-impl App {
-    fn new() -> Self {
+impl Renderer {
+    fn new(width: u32, height: u32) -> Self {
         let font = include_bytes!("../resources/NotoSansCJKjp-Regular.otf") as &[u8];
         let settings = fontdue::FontSettings {
             scale: 200.0,
@@ -24,6 +31,49 @@ impl App {
 
         Self {
             font: font,
+            buffer: vec![u32::MAX; (width * height) as usize],
+            width: width,
+            height: height,
+        }
+    }
+
+    fn draw(&mut self, buffer: &mut [u32], width: u32, height: u32){
+        self.width = width;
+        self.height = height;
+        self.buffer = vec![u32::MAX; (width * height) as usize];
+
+        self.draw_char('k', 100, 100);
+
+        for (i, pixel) in buffer.iter_mut().enumerate() {
+            let x = i % width as usize;
+            let y = i / width as usize;
+
+            *pixel = self.pixel_at(x, y);
+        }
+    }
+
+    fn draw_char(&mut self, character: char, x: usize, y: usize) {
+        let (metrics, bitmap) = self.font.rasterize(character, 200.0);
+
+        for (i, bit) in bitmap.iter().enumerate() {
+            let c = 255 - *bit;
+
+            let x: usize = i % metrics.width + x;
+            let y: usize = i / metrics.width + y;
+
+            self.buffer[y * self.width as usize + x] = rgb(c, c, c);
+        }
+    }
+
+    fn pixel_at(&self, x: usize, y: usize) -> u32{
+        self.buffer[y * self.width as usize + x]
+    }
+}
+
+impl App {
+    fn new() -> Self {
+        Self {
+            renderer: None,
             state: None
         }
     }
@@ -36,12 +86,13 @@ impl ApplicationHandler for App {
         let context = softbuffer::Context::new(Arc::clone(&window)).unwrap();
         let surface = softbuffer::Surface::new(&context, Arc::clone(&window)).unwrap();
 
+        self.renderer = Some(Renderer::new(window.inner_size().width, window.inner_size().height));
         self.state = Some((window, surface));
     }
     fn window_event(
         &mut self,
         event_loop: &ActiveEventLoop,
-        window_id: winit::window::WindowId,
+        _window_id: winit::window::WindowId,
         event: WindowEvent,
     )
     {
@@ -52,6 +103,10 @@ impl ApplicationHandler for App {
             },
             WindowEvent::Resized(_) | WindowEvent::RedrawRequested => {
                 let Some((window, surface)) = self.state.as_mut() else {
+                    return;
+                };
+
+                let Some(renderer)  = self.renderer.as_mut() else {
                     return;
                 };
 
@@ -69,31 +124,12 @@ impl ApplicationHandler for App {
 
                 let mut buf = surface.buffer_mut().unwrap();
 
-                draw(&mut buf, w);
+                renderer.draw(&mut buf, w, h);
                 
-                draw_char(&mut buf, w, &self.font, 'k');
-
                 buf.present().unwrap();
             }
             _ => (),
         }
-    }
-}
-
-fn draw(buffer:&mut [u32], width: u32) {
-    buffer.fill(rgb(255, 255, 255));
-}
-
-fn draw_char(buffer: &mut [u32], width: u32, font: &Font, character: char) {
-    let (metrics, bitmap) = font.rasterize(character, 200.0);
-
-    for (i, bit) in bitmap.iter().enumerate() {
-        let c = 255 - *bit;
-
-        let x: usize = i % metrics.width;
-        let y: usize = i / metrics.width;
-
-        buffer[y * width as usize + x] = rgb(c, c, c);
     }
 }
 
