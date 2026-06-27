1bc8e1652f4b47746a5f6c65fafe01138b66f960
k1mqq <keemacurry238@gmail.com>
2026-06-28 07:32:52 +0900

    inline box

--- src/layout.rs
+++ src/layout.rs
@@ -1,3 +1,5 @@
+use std::default;
+
 use fontdue::{Font, layout::{CoordinateSystem, TextStyle}};
 
 use crate::html_parser::{Dom, NodeId, NodeType};
@@ -15,6 +17,13 @@ pub struct LayoutContext<'a> {
 pub struct Component {
     pub dimentions: Dimentions,
     pub text: Option<String>,
+    pub box_type: BoxType,
+}
+
+#[derive(Clone)]
+pub enum BoxType {
+    Block,
+    Inline
 }
 
 #[derive(Default)]
@@ -90,33 +99,10 @@ fn next_component(components: &mut Vec<Component>, dom: &Dom, node_id: NodeId, p
     let node = &dom.nodes[node_id];
     let id = components.len();
 
-    let (x, y) = match brother {
-        Some(brother_id) => {(
-            components[brother_id].dimentions.content.x,
-            components[brother_id].dimentions.content.y + components[brother_id].dimentions.outer_height()
-        )}
-        None => {
-            match parent {
-                Some(parent_id) => {(
-                    components[parent_id].dimentions.content.x + components[parent_id].dimentions.padding.left,
-                    components[parent_id].dimentions.content.y + components[parent_id].dimentions.padding.top
-                )}
-                None => (0.0, 0.0)
-            }
-        }
-    };
-
-    let width = match parent {
-        Some(parent_id) => {
-            components[parent_id].dimentions.content.width
-        }
-        None => {
-            context.window_width as f32
-        }
-    };
-
-    match &node.node_type {
+    let (margin, text, box_type) = match &node.node_type {
         NodeType::Element { tag, attributes } => {
+            let mut margin = EdgeSize::default();
+            let mut box_type = BoxType::Block;
             if matches!(tag.as_str(), "script" | "style"){
                 return None;
             }
@@ -125,79 +111,132 @@ fn next_component(components: &mut Vec<Component>, dom: &Dom, node_id: NodeId, p
                     return None;
                 }
                 "body" => {
-                    Dimentions {
-                        //                     :(          :(
-                        content: Rect { x: x + 8.0, y: y + 8.0, width: width - 8.0, height: 20.0},
-                        margin: EdgeSize {
-                            left: 8.0,
-                            right: 8.0,
-                            top: 8.0,
-                            bottom: 8.0,
-                        },
-                        ..Default::default()
-                    }
+                    margin = EdgeSize { left: 8.0, right: 8.0, top: 8.0, bottom: 8.0 };
                 }
-                "h1" => {
-                    Dimentions {
-                        content: Rect { x: x, y: y, width: width, height: 20.0 },
-                        ..Default::default()
-                    }
-                }
-                "p" | "a" | "span"=> {
-                    Dimentions {
-                        content: Rect { x: x, y: y, width: width, height: 10.0 },
-                        ..Default::default()
-                    }
+                "a" | "span"=> {
+                    box_type = BoxType::Inline;
                 }
                 _ => {
-                    Dimentions {
-                        content: Rect{ x: x, y: y, width: width, height: 0.0},
-                        ..Default::default()
-                    }
                 }
             };
-            components.push(Component {
-                dimentions: dimentions,
-                text: None,
-            });
+            (margin, None, box_type)
         }
         NodeType::Text(text) => {
-            // recreate every time for no reason
-            // fix later
+            (EdgeSize::default(), Some(text.to_string()), BoxType::Inline)
+        }
+        _ => {
+            return None;
+        }
+    };
+
+    let (x, y) = match box_type {
+        BoxType::Block => {
+            match brother {
+                Some(brother_id) => {(
+                    components[brother_id].dimentions.content.x,
+                    components[brother_id].dimentions.content.y + components[brother_id].dimentions.outer_height()
+                )}
+                None => {
+                    match parent {
+                        Some(parent_id) => {(
+                            components[parent_id].dimentions.content.x + components[parent_id].dimentions.padding.left,
+                            components[parent_id].dimentions.content.y + components[parent_id].dimentions.padding.top
+                        )}
+                        None => (0.0, 0.0)
+                    }
+                }
+            }
+        }
+        BoxType::Inline => {
+            match brother {
+                Some(brother_id) => {(
+                    // imperfect
+                    components[brother_id].dimentions.content.x + components[brother_id].dimentions.outer_width(),
+                    components[brother_id].dimentions.content.y
+                )}
+                None => {
+                    match parent {
+                        Some(parent_id) => {(
+                            components[parent_id].dimentions.content.x + components[parent_id].dimentions.padding.left,
+                            components[parent_id].dimentions.content.y + components[parent_id].dimentions.padding.top
+                        )}
+                        None => (0.0, 0.0)
+                    }
+                }
+            }
+        }
+    };
+
+    let width = match box_type {
+        BoxType::Block => {
+            match parent {
+                Some(parent_id) => {
+                    components[parent_id].dimentions.content.width
+                }
+                None => {
+                    context.window_width as f32
+                }
+            }
+        }
+        BoxType::Inline => {
+            match &text {
+                Some(text) => {
+                    text.chars().map(|c| {
+                        let metrics = context.font.metrics(c, 20.0);
+                        metrics.advance_width.ceil()
+                    }).sum()
+                }
+                None => 0.0
+            }
+        }
+    };
+
+    let height = match &text {
+        Some(text) => {
             let mut font_layout = fontdue::layout::Layout::new(CoordinateSystem::PositiveYDown);
             let font_layout_settings = fontdue::layout::LayoutSettings {
-                max_width: Some(width),
+                // max_width: Some(width),
                 ..Default::default()
             };
             font_layout.reset(&font_layout_settings);
 
             font_layout.append(&[context.font], &TextStyle::new(&text, 20.0, 0));
+            font_layout.height()
+        }
+        None => 0.0
+    };
 
-            components.push(Component {
-                dimentions: Dimentions {
-                    content: Rect { x: x, y: y, width: width, height: font_layout.height() },
-                    padding: EdgeSize::default(),
-                    border: EdgeSize::default(),
-                    margin: EdgeSize::default(),
-                },
-                text: Some(text.to_string()),
-            });
-        }
-        _ => {
-            // ;)
-        }
-    }
+    components.push(Component {
+        dimentions: Dimentions {
+            content: Rect{ x: x + margin.left, y: y + margin.top, width: width, height: height },
+            margin: margin,
+            ..Default::default()
+        },
+        text: text,
+        box_type: box_type.clone()
+    });
 
     let mut last_child = None;
     let mut height = components[id].dimentions.content.height;
+    let mut width = components[id].dimentions.content.width;
     for child_node in &node.children {
         let Some(child) = next_component(components, dom, *child_node, Some(id), last_child, context) else {
             continue;
         };
-        height += components[child].dimentions.outer_height();
+        match box_type {
+            BoxType::Block => {
+                height += components[child].dimentions.outer_height();
+                width = width.max(components[child].dimentions.outer_width());
+            }
+            BoxType::Inline => {
+                width += components[child].dimentions.outer_width();
+                height = height.max(components[child].dimentions.outer_height());
+            }
+        }
         last_child = Some(child);
     }
 
+    components[id].dimentions.content.width = width;
     components[id].dimentions.content.height = height;
 
     return Some(id);
