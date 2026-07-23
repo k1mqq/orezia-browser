1586478457a5a79ca9edb208713537f1520c161f
k1mqq <keemacurry238@gmail.com>
2026-07-23 18:09:21 +0900

    colorful font

--- src/renderer.rs
+++ src/renderer.rs
@@ -5,6 +5,36 @@ use fontdue::{Font, Metrics};
 
 use crate::layout::Layout;
 
+struct Rect {
+    x: usize,
+    y: usize,
+    width: usize,
+    height: usize,
+}
+
+#[derive(Clone, Copy)]
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
+
+impl From<Color> for u32 {
+    fn from(color: Color) -> Self {
+        ((color.r as u32) << 16) | ((color.g as u32) << 8) | (color.b as u32)
+    }
+}
+
+impl From<u32> for Color {
+    fn from(n: u32) -> Self {
+        Color {
+            r: (n >> 16 & 0xff) as u8,
+            g: (n >> 8 & 0xff) as u8,
+            b: (n & 0xff) as u8,
+        }
+    }
+}
+
 pub struct Renderer {
     buffer: Vec<u32>,
     pub fonts: Vec<Font>,
@@ -42,7 +72,21 @@ impl Renderer {
         self.height = height;
         self.buffer = vec![u32::MAX; (width * height) as usize];
 
-        self.draw_string("Orezia by k1mq!".to_string(), 100, 200, 1000, 100.0);
+        self.draw_string(
+            "Orezia by k1mq!".to_string(),
+            Rect {
+                x: 100,
+                y: 200,
+                width: 1000,
+                height: 100,
+            },
+            100.0,
+            Color {
+                r: 0,
+                g: 200,
+                b: 200,
+            },
+        );
         self.draw_layout(layout);
 
         for (i, pixel) in buffer.iter_mut().enumerate() {
@@ -54,7 +98,7 @@ impl Renderer {
     }
 
     fn draw_layout(&mut self, layout: Layout) {
-        let draw_calls: Vec<(String, usize, usize, usize)> = layout
+        let draw_calls: Vec<(String, Rect)> = layout
             .components
             .iter()
             .filter(|c| {
@@ -65,9 +109,12 @@ impl Renderer {
                 if let Some(text) = &c.text {
                     Some((
                         text.clone(),
-                        c.dimentions.content.x as usize,
-                        c.dimentions.content.y as usize,
-                        c.dimentions.content.width as usize,
+                        Rect {
+                            x: c.dimentions.content.x as usize,
+                            y: c.dimentions.content.y as usize,
+                            width: c.dimentions.content.width as usize,
+                            height: c.dimentions.content.height as usize,
+                        },
                     ))
                 } else {
                     None
@@ -75,8 +122,8 @@ impl Renderer {
             })
             .collect();
 
-        for (text, x, y, width) in draw_calls {
-            self.draw_string(text, x, y, width, 20.0);
+        for (text, text_box) in draw_calls {
+            self.draw_string(text, text_box, 20.0, Color { r: 0, g: 0, b: 0 });
         }
     }
 
@@ -95,31 +142,35 @@ impl Renderer {
     //     }
     // }
 
-    fn draw_string(&mut self, string: String, x: usize, y: usize, width: usize, size: f32) {
+    fn draw_string(&mut self, text: String, text_box: Rect, size: f32, color: Color) {
         let layout_settings = LayoutSettings {
-            max_width: Some(width as f32),
+            max_width: Some(text_box.width as f32),
             ..Default::default()
         };
         self.font_layout.reset(&layout_settings);
         // no longer cloned font
         self.font_layout
-            .append(self.fonts.as_slice(), &TextStyle::new(&string, size, 0));
+            .append(self.fonts.as_slice(), &TextStyle::new(&text, size, 0));
         for glyph in self.font_layout.glyphs() {
             let (metrics, bitmap) = self
                 .font_cache
                 .entry(glyph.key)
                 .or_insert_with(|| self.fonts[0].rasterize_config(glyph.key));
             for (i, bit) in bitmap.iter().enumerate() {
-                let c = 255 - *bit;
-
-                let x: usize = i % metrics.width + x as usize + glyph.x as usize;
-                let y: usize = i / metrics.width + y as usize + glyph.y as usize;
-
-                if x >= self.width as usize || y >= self.height as usize {
-                    break;
+                if *bit == 0 {
+                    continue;
                 }
 
-                self.buffer[y * self.width as usize + x] = rgb(c, c, c);
+                let x: usize = i % metrics.width + text_box.x as usize + glyph.x as usize;
+                let y: usize = i / metrics.width + text_box.y as usize + glyph.y as usize;
+
+                if x >= self.width as usize || y >= self.height as usize {
+                    continue;
+                }
+
+                let index = y * self.width as usize + x;
+
+                self.buffer[index] = blend(color, Color::from(self.buffer[index]), *bit).into();
             }
         }
     }
@@ -129,6 +180,17 @@ impl Renderer {
     }
 }
 
-fn rgb(r: u8, g: u8, b: u8) -> u32 {
-    ((r as u32) << 16) | ((g as u32) << 8) | (b as u32)
+fn blend(fg: Color, bg: Color, a: u8) -> Color {
+    let a = a as u16;
+    let inv_a = 255 - a;
+
+    let r = (fg.r as u16 * a + bg.r as u16 * inv_a) / 255;
+    let g = (fg.g as u16 * a + bg.g as u16 * inv_a) / 255;
+    let b = (fg.b as u16 * a + bg.b as u16 * inv_a) / 255;
+
+    Color {
+        r: r as u8,
+        g: g as u8,
+        b: b as u8,
+    }
 }
